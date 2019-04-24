ea85bdfead6d3ea5d072bed1780380f63c862c21
Ubuntu <ubuntu@ip-172-31-53-80.us-west-2.compute.internal>
2019-04-24 13:28:04 +0000

    Bash script to download imagenet

---
+++ scripts/download_imagenet.sh
@@ -0,0 +1,10 @@
+function gdrive_download () {
+  CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
+  wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2
+  
+rm -rf /tmp/cookies.txt
+}
+
+gdrive_download 0B3Irx3uQNoBMQ1FlNXJsZUdYWEE ../dataset/imagenet/images.zip
+
+unzip -uq images.zip && rm images.zip

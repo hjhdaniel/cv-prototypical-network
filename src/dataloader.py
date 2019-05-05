c53dec3548eeb29d1f96aa936d8c274849c92b42
hjhdaniel <daniel.ho.jin.hao@gmail.com>
2019-05-05 23:41:09 +0800

    updated_gaussian

--- src/dataloader.py
+++ src/dataloader.py
@@ -9,7 +9,14 @@ import os, torch
 import numpy as np
 
 from omniglot_dataset import OmniglotDataset
+from mini_imagenet_dataset import MiniImageNet
+from cub200_dataset import CUB2002010
+# from cub2011_dataset import CUB2011
 from batch_sampler import BatchSampler
+
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
 
 class DataLoader(object):
 
@@ -19,26 +26,82 @@ class DataLoader(object):
         self.data_loader = self.set_data_loader()
 
     def data_loader(self):
-        return self.__data_loader
+        return self.data_loader
 
     def set_data_loader(self):
 
-        if self.arg_settings.data=='omniglot':
-            dataset = OmniglotDataset(mode=self.mode, root=self.arg_settings.dataset_root)
+        if self.arg_settings.data == 'omniglot':
+            dataset = OmniglotDataset(mode=self.mode, root=self.arg_settings.dataset_root + '/omniglot/')
             num_classes = len(np.unique(dataset.y))
             sampler = self.create_sampler(dataset.y)
 
+        elif self.arg_settings.data == 'imagenet':
+            dataset = MiniImageNet(mode=self.mode, root=self.arg_settings.dataset_root + '/imagenet/')
+            num_classes = len(np.unique(dataset.label))
+            sampler = self.create_sampler(dataset.label)
+            print('num_classes: ', num_classes, ' for ', self.mode)
+            print('Number of data: ', len(dataset))
+
+        elif self.arg_settings.data == 'cub200':
+            trans = transforms.Compose([
+                transforms.Resize(64),
+                transforms.CenterCrop(64),
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ])
+            dataset = CUB2002010(root=self.arg_settings.dataset_root + '/cub200/', mode=self.mode, transform=trans)
+            num_classes = len(np.unique(dataset.labels))
+            sampler = self.create_sampler(dataset.labels)
+            print('num_classes: ', num_classes, ' for ', self.mode)
+            print('Num of data', len(dataset))
+
+        elif self.arg_settings.data == 'cub2011':
+            trans = transforms.Compose([
+                transforms.Resize(64),
+                transforms.CenterCrop(64),
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ])
+            dataset = CUB2011(root=self.arg_settings.dataset_root + '/CUB_200_2011/', transform=trans)
+            # print(dataset.labels)
+            num_classes = len(np.unique(dataset.labels))
+            sampler = self.create_sampler(dataset.labels)
+            print('num_classes: ', num_classes, ' for ', self.mode)
+            print('Num of data', len(dataset))
+
+        elif self.arg_settings.data == 'CIFAR':
+            trans = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            ])
+            if self.mode == "train":
+                dataset = datasets.CIFAR100(root=self.arg_settings.dataset_root + '/CIFAR/', train=True,
+                                            transform=trans, target_transform=None, download=True)
+            else:
+                dataset = datasets.CIFAR100(root=self.arg_settings.dataset_root + '/CIFAR/', train=False,
+                                            transform=trans, target_transform=None, download=True)
+            num_classes = len(np.unique(dataset.targets))
+            sampler = self.create_sampler(dataset.targets)
+            print('num_classes: ', num_classes, ' for ', self.mode)
+            print('Num of data', len(dataset))
+
         # check if number of classes in dataset is sufficient
-        if num_classes < self.arg_settings.classes_per_it_tr or num_classes < self.arg_settings.classes_per_it_val:
-            raise(Exception('There are not enough classes in the dataset in order ' +
-                            'to satisfy the chosen classes_per_it. Decrease the ' +
-                            'classes_per_it_{tr/val} option and try again.'))
+        if self.mode == 'train':
+            if (num_classes < self.arg_settings.classes_per_it_tr):
+                raise (Exception('There are not enough classes in the dataset in order ' +
+                                 'to satisfy the chosen classes_per_it. Decrease the ' +
+                                 'classes_per_it_tr option and try again.'))
+        else:
+            if num_classes < self.arg_settings.classes_per_it_val:
+                raise (Exception('There are not enough classes in the dataset in order ' +
+                                 'to satisfy the chosen classes_per_it. Decrease the ' +
+                                 'classes_per_it_val option and try again.'))
 
         dataloader = torch.utils.data.DataLoader(dataset, batch_sampler=sampler)
         return dataloader
 
     def create_sampler(self, labels):
-        if self.mode=='train':
+        if self.mode == 'train':
             classes_per_it = self.arg_settings.classes_per_it_tr
             num_samples = self.arg_settings.num_support_tr + self.arg_settings.num_query_tr
         else:
